eec67425aaa531e6a5d30aaf0940eab2f7eb3d4d
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-03 16:32:27 -0600

    section 1

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 // import functions and grab DOM elements
-const addIngredient = document.getElementById(food);
-const addQuantity = document.getElementById(amount);
-const addMeasure = document.getElementById(measure);
-
-const form = document.
-
+const recipe = document.getElementById('list'); 
+const form = document.getElementById('ingredient-list');
+const button = document.getElementById('add');
 
 // let state
-
+let ingredientArray = [];
+console.log(ingredientArray);
 
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const totalList = new FormData(form);
+
+    let ingredients = {
+        name: totalList.get('food'),
+        quantity: totalList.get('amount'),
+        measurement: totalList.get('measure'),
+    };
+    ingredientArray.push(ingredients);
+    console.log(ingredientArray);
+});
